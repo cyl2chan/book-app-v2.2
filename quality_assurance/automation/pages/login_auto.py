9826c85a6fa2b5e34547823370d06141da3e7f01
Leon Chan <102279479+cyl2chan@users.noreply.github.com>
2026-04-13 12:23:36 -0400

    completed finding locators of input field of email and password in login_auto.py

--- quality_assurance/automation/pages/login_auto.py
+++ quality_assurance/automation/pages/login_auto.py
@@ -2,7 +2,12 @@
 This module contains the page object for the login page.
 """
 
+from selenium.webdriver.common.by import By
+
 class LoginPageAuto:
+
+    EMAIL_INPUT = (By.ID, "username")
+    PASSWORD_INPUT = (By.ID, "password")
 
     def __init__(self, browser):
         self.broswer = browser
