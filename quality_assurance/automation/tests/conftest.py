705823a2b593854da016532f3052f41a888f3134
Leon Chan <102279479+cyl2chan@users.noreply.github.com>
2026-04-15 09:21:50 -0400

    Added code in conftest.py for configuring multiple browsers

--- quality_assurance/automation/tests/conftest.py
+++ quality_assurance/automation/tests/conftest.py
@@ -9,7 +9,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-
 # set up options for webdriver
 options = Options()
 options.add_argument('--headless')
@@ -17,15 +16,38 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
 @pytest.fixture
-def browser():
+def config(scope='session'):
 
-    # Initialise the ChromeDriver instance
-    service = Service ("/usr/bin/chromedriver")
-    #b = selenium.webdriver.Chrome()
-    b = webdriver.Chrome(service=service, options=options)
+    # Read the file
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+    print("config file opened")
+
+    # Assert values are acceptable
+    assert config['browser'] in ['Firefox', 'Chrome', 'Headless Chrome']
+    assert config['implicit_wait'] > 0
+    print("assert passed")
+    
+    # Return config so it can be used
+    return config
+
+
+@pytest.fixture
+def browser(config):
+
+    # Initialise tbe WebDriver instance
+    if config['browser'] == 'Firefox':
+        b = selenium.webdriver.Firefox()
+        print("firefox browser")
+    elif config['browser'] == 'Headless Chrome':
+        print("Headless Chrome browser")
+        service = Service ("/usr/bin/chromedriver")
+        b = webdriver.Chrome(service=service, options=options)
+    else: 
+        raise Exception(f'Browser "{config["browser"]}" is not supported')
 
     # Make its calls wait up to 10 seconds for elements to appear
-    b.implicitly_wait(10)
+    b.implicitly_wait(config['implicit_wait'])
 
     # Return the WebDriver instance for the setup
     yield b 
