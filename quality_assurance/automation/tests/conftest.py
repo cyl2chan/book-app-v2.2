084c398fc8b111451609782c33999ad1d2f3498d
Leon Chan <102279479+cyl2chan@users.noreply.github.com>
2026-04-16 12:19:08 -0400

    fix code for passing test in Firefox in conftest.py

--- quality_assurance/automation/tests/conftest.py
+++ quality_assurance/automation/tests/conftest.py
@@ -6,14 +6,20 @@ import json
 import pytest
 import selenium.webdriver
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options as chrome_Options
+from selenium.webdriver.chrome.service import Service as chrome_Service
+from selenium.webdriver.firefox.options import Options as firefox_Options
+from selenium.webdriver.firefox.service import Service as firefox_Service
 
-# set up options for webdriver
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+# set up options for google chrome webdriver
+chrome_options = chrome_Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+# Firefox
+firefox_options = firefox_Options()
+
 
 @pytest.fixture
 def config(scope='session'):
@@ -37,12 +43,11 @@ def browser(config):
 
     # Initialise tbe WebDriver instance
     if config['browser'] == 'Firefox':
-        b = selenium.webdriver.Firefox()
-        print("firefox browser")
+        firefox_service = firefox_Service("/snap/bin/geckodriver")
+        b = webdriver.Firefox(service=firefox_service, options=firefox_options)
     elif config['browser'] == 'Headless Chrome':
-        print("Headless Chrome browser")
-        service = Service ("/usr/bin/chromedriver")
-        b = webdriver.Chrome(service=service, options=options)
+        chrome_service = chrome_Service("/usr/bin/chromedriver")
+        b = webdriver.Chrome(service=chrome_service, options=chrome_options)
     else: 
         raise Exception(f'Browser "{config["browser"]}" is not supported')
 
